cecb8254cad2c4578ed94189c345c12573410df0
Vivek Raj Roy <83235714+unknownforall1@users.noreply.github.com>
2023-12-31 15:46:06 +0530

    Update config.py

--- FallenRobot/config.py
+++ FallenRobot/config.py
@@ -2,27 +2,27 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    API_ID = 6
+    API_ID = "7988735"
     API_HASH = "eb06d4abfb49dc3eeb1aeb98ae0f581e"
 
     CASH_API_KEY = "3FTOVT1DG6S1K288"  # Get this value for currency converter from https://www.alphavantage.co/support/#api-key
 
-    DATABASE_URL = ""  # A sql database url from elephantsql.com
+    DATABASE_URL = "postgres://mowrxiys:wXj2GiN0ymdoQzMzPriRms-uJL1TQUii@mahmud.db.elephantsql.com/mowrxiys"  # A sql database url from elephantsql.com
 
     EVENT_LOGS = ()  # Event logs channel to note down important bot level events
 
-    MONGO_DB_URI = ""  # Get ths value from cloud.mongodb.com
+    MONGO_DB_URI = "mongodb+srv://vivekrajroy705:tGPeMA1SmTO8yRIE@cluster0.zse4xpv.mongodb.net/?retryWrites=true&w=majority"  # Get ths value from cloud.mongodb.com
 
     # Telegraph link of the image which will be shown at start command.
     START_IMG = "https://te.legra.ph/file/40eb1ed850cdea274693e.jpg"
 
     SUPPORT_CHAT = "DevilsHeavenMF"  # Your Telegram support group chat username where your users will go and bother you
 
-    TOKEN = ""  # Get bot token from @BotFather on Telegram
+    TOKEN = "6378838969:AAEYwlWak7IHb7CDdZD4SXJWJWopdNt-XWo"  # Get bot token from @BotFather on Telegram
 
     TIME_API_KEY = ""  # Get this value from https://timezonedb.com/api
 
-    OWNER_ID = 1356469075  # User id of your telegram account (Must be integer)
+    OWNER_ID = 6134091518  # User id of your telegram account (Must be integer)
 
     # Optional fields
     BL_CHATS = []  # List of groups that you want blacklisted.
